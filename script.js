a15b7b34c73a1d289d56b0a97e263da376c6d3af
Ilya Petrin <125139653+DrHentain@users.noreply.github.com>
2025-03-18 20:08:21 +0300

    README
    Add README files, deleted shop.html

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Здесь можно добавить JavaScript для интерактивности, если нужно
-//Например, для плавного скролла к секциям
+//Для плавного скролла к секциям
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
